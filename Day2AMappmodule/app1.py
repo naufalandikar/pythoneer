54520f5af6d96e9dde28836b5b4a08b7fa48b203
Naufal Andika <andikanaufal4@gmail.com>
2023-12-13 18:40:40 +0700

    exerciseDay3

---
+++ Day2AMappmodule/app1.py
@@ -0,0 +1,7 @@
+import Day2AMappmodule.module1 as mod1
+
+fahrenheit = mod1.celcius_to_fahr(25)
+kelvin = mod1.fahr_to_kelvin(fahrenheit)
+
+print(fahrenheit)
+print(kelvin)
---
+++ Day2AMappmodule/module1.py
@@ -0,0 +1,4 @@
+def celcius_to_fahr(temp):
+    return 9/5 * temp + 32
+def fahr_to_kelvin(temp):
+    return 9 * temp + 20
